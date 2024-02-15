977ca32c70055025816d3b764d9d4d8aec9878f8
R1nge <r1ngegamedev@gmail.com>
2024-02-15 08:34:40 +0300

    Bot editor (WIP)

---
+++ Assets/_Assets/Scripts/Services/UIs/BotEditor/BotEditorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using _Assets.Scripts.Gameplay.Parts;
+using _Assets.Scripts.Services.BotEditor;
+using UnityEngine;
+using VContainer;
+
+namespace _Assets.Scripts.Services.UIs.BotEditor
+{
+    public class BotEditorController : MonoBehaviour
+    {
+        [SerializeField] private BotEditorView botEditorView;
+        [Inject] private BotEditorService _botEditorService;
+
+        private void Start()
+        {
+            botEditorView.OnCreate += Create;
+        }
+
+        private void Create(PartData.PartType type)
+        {
+            _botEditorService.Spawn(Vector3.zero, type);
+        }
+
+        private void OnDestroy()
+        {
+            botEditorView.OnCreate -= Create;
+        }
+    }
+}
---
+++ Assets/_Assets/Scripts/Services/UIs/BotEditor/BotEditorView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using _Assets.Scripts.Gameplay.Parts;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Assets.Scripts.Services.UIs.BotEditor
+{
+    public class BotEditorView : MonoBehaviour
+    {
+        [SerializeField] private Button createWheel;
+        public event Action<PartData.PartType> OnCreate;
+
+        private void Start() => createWheel.onClick.AddListener(CreateSmallWheel);
+
+        private void CreateSmallWheel() => OnCreate?.Invoke(PartData.PartType.SmallWheel);
+    }
+}
