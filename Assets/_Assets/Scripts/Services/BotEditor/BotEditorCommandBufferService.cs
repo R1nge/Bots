a3528937b5989d00b94ed986bbf8422b8224ef2b
R1nge <r1ngegamedev@gmail.com>
2024-02-19 13:36:17 +0300

    Editor undo - limit commands count

--- Assets/_Assets/Scripts/Services/BotEditor/BotEditorCommandBufferService.cs
+++ Assets/_Assets/Scripts/Services/BotEditor/BotEditorCommandBufferService.cs
@@ -6,31 +6,35 @@ namespace _Assets.Scripts.Services.BotEditor
 {
     public class BotEditorCommandBufferService
     {
-        private readonly Stack<IEditorCommand> _commands = new();
-        private readonly Stack<IEditorCommand> _undoCommands = new();
+        private readonly CircularBuffer<IEditorCommand> _commands = new(10);
+        private readonly CircularBuffer<IEditorCommand> _undoCommands = new(10);
 
-        public bool HasCommands() => _commands.Count > 0;
-        public bool HasUndoCommands() => _undoCommands.Count > 0;
+        public bool HasCommands() => _commands.Size > 0;
+        public bool HasUndoCommands() => _undoCommands.Size > 0;
+        
+        //TODO: limit command count
 
         public void Execute(IEditorCommand command)
         {
             Debug.Log("Add");
             command.Execute();
-            _commands.Push(command);
+            _commands.PushBack(command);
         }
 
         public void Undo()
         {
-            var command = _commands.Pop();
+            var command = _commands.Back();
+            _commands.PopBack();
             command.Undo();
-            _undoCommands.Push(command);
+            _undoCommands.PushBack(command);
         }
 
         public void Redo()
         {
-            var command = _undoCommands.Pop();
+            var command = _undoCommands.Back();
+            _undoCommands.PopBack();
             command.Execute();
-            _commands.Push(command);
+            _commands.PushBack(command);
         }
     }
 }
